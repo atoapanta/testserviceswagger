601611ceab1402aaf749c4fe6089b9c7a2c08f60
Alexander Toapanta <dev.xandert@gmail.com>
2024-10-23 23:01:00 -0500

    start

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+const app = express();
+const PORT = 3000;
+
+// Ruta raíz
+app.get("/", (req, res) => {
+  res.send("¡Hola, mundo desde Node.js!");
+});
+
+// Ruta para servir archivos estáticos desde la carpeta public
+// app.use(express.static(path.join(process.cwd(), "public")));
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
